fe0a4a8a55cda7173f198a9488717efaa98e75c8
Nwekar <nwekar.mahdi89@gmail.com>
2024-06-03 02:10:29 +0300

    Update Single quotation

--- app/page.tsx
+++ app/page.tsx
@@ -25,13 +25,13 @@ const Home: React.FC<Iprops> = (props) => {
         transition={{ type: 'tween', ease: 'backOut', duration: 0.5 }}
       >
         <p onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-          A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+          A visual designer - with skills that haven&apos;t been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
         </p>
       </motion.div>
 
       <div className={styles.body}>
         <p>
-          I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.
+          I&apos;m a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.
         </p>
       </div>
     </main>
